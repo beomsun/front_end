86bb0fb61b33f5b6768ee385271305c787273051
beomsun <dudn377@gmail.com>
2016-07-22 17:55:13 +0900

    no message

---
+++ class/webpage/js/gnb.js
@@ -0,0 +1,18 @@
+/**
+ * Created by 321-07 on 2016-07-22.
+ */
+
+
+$(function(){
+
+    $('.gnb-depth1-link').on('mouseenter', function(){
+        $('.gnb-depth2').show();
+        $('.gnb-bg').show();
+    });
+
+    $('.gnb-depth1-link').on('mouseleave', function(){
+        $('.gnb-depth2').hide();
+        $('.gnb-bg').hide();
+    });
+
+});
